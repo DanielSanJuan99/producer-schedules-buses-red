b724fbff533614fe8014960a6d6c05fa3b10ad45
Diego Andrés Muñoz Morales <86175879+DimmoX@users.noreply.github.com>
2026-02-15 03:10:57 -0300

    feat: configurar aplicación y conexión RabbitMQ
    - Agregar application.yml con configuración de RabbitMQ
    - Agregar punto de entrada ProducerSchedulesApplication

---
+++ src/main/java/com/busesred/producer/schedules/ProducerSchedulesApplication.java
@@ -0,0 +1,11 @@
+package com.busesred.producer.schedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerSchedulesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerSchedulesApplication.class, args);
+    }
+}
